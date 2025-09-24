1ed5dd34768f84fd41df278f3c164bf7a2859c48
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-24 12:26:19 +0300

    update newsapp models

--- newsapp/models.py
+++ newsapp/models.py
@@ -5,6 +5,7 @@ class Writer(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True, null=True)
     email = models.EmailField(unique=True)
+    writer_profile_pic = CloudinaryField('image', blank=True, null=True)
     
     def __str__(self):
         return self.name
@@ -26,3 +27,13 @@ class NewsArticle(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+class Comment(models.Model):
+    article = models.ForeignKey(NewsArticle, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author_name} on {self.article.title}'
