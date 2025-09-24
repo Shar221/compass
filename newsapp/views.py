23daab497417b494b1764fae20a950c901a1e149
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-24 12:54:40 +0300

    display news articles

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import NewsArticle, Writer, Category, Comment
 
 def index(request):
-    return render(request, 'newsapp/index.html')
+    articles = NewsArticle.objects.all().order_by('-published_at')[:5]
+    context = {'articles': articles}    
+    
+    return render(request, 'newsapp/index.html', context)
+
+def article_detail(request, article_id):
+    article = NewsArticle.objects.get(id=article_id)
+    comments = article.comments.all().order_by('-created_at')
+    context = {'article': article, 'comments': comments}
+    
+    return render(request, 'newsapp/article_detail.html', context)  
