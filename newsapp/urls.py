af4e3a8f0e6444146aab99fb68db3f57bfbd0c09
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-24 13:07:49 +0300

    add news detail

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, article_detail
 
 urlpatterns = [
     path('', index, name='index'),
+    path('article/<int:article_id>/', article_detail, name='article_detail'),
 ]
 
