54504e187fb544b073b06f037019b9ffd3d001be
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-25 10:13:02 +0300

    add registration template

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
+from django.views import View
+from django.contrib.auth import login
 
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, 'accounts/register.html', {'user_form': form})
+    
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('profile')
+        return render(request, 'accounts/register.html', {'user_form': form})
+        
