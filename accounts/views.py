97ad17b25c7f49205425e58a4c0f0964d6526fc0
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-25 10:27:55 +0300

    add login

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib import messages
 from django.views import View
 from django.contrib.auth import login
+from django.contrib.auth import authenticate, logout
 
 class RegisterView(View):
     def get(self, request):
@@ -13,8 +14,31 @@ class RegisterView(View):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
             messages.success(request, 'Registration successful. You can now log in.')
-            return redirect('profile')
+            return redirect('login')
         return render(request, 'accounts/register.html', {'user_form': form})
-        
+
+class LoginView(View):
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, 'accounts/login.html', {'login_form': form})
+    
+    def post(self, request):
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Login successful.')
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid email or password.')
+        return render(request, 'accounts/login.html', {'login_form': form})
+    
+class logoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You have been logged out.')
+        return redirect('login')
