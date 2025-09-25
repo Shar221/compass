01809d6ed95b664d82a22ca2ba63802e6828dd38
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-25 11:12:06 +0300

    add create article form

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   
 from .models import NewsArticle, Category, Comment
+from .forms import NewsArticleForm, UpdateNewsArticleForm
 
 def index(request):
     articles = NewsArticle.objects.all().order_by('-published_at')[:5]
@@ -14,3 +15,27 @@ def article_detail(request, article_id):
     
     return render(request, 'newsapp/article_detail.html', context)
 
+def create_news_article(request):
+    if request.method == 'POST':
+        form = NewsArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            news_article = form.save(commit=False)
+            news_article.writer = request.user
+            news_article.save()
+            return redirect('index')
+    else:
+        form = NewsArticleForm()
+    
+    return render(request, 'newsapp/create_article.html', {'form': form})
+
+def update_news_article(request, article_id):
+    article = NewsArticle.objects.get(id=article_id)
+    if request.method == 'POST':
+        form = UpdateNewsArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_detail', article_id=article.id)
+    else:
+        form = UpdateNewsArticleForm(instance=article)
+    
+    return render(request, 'newsapp/update_article.html', {'form': form, 'article': article})
